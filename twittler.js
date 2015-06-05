e41e4eea28d768d00a04e3c3dea04e02d846b6d3
Charles Crame <ccrame@gmail.com>
2015-06-05 14:59:56 -0700

    Added text input field, more styling fixes

--- twittler.js
+++ twittler.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   var $body = $('.tweet-board');
-  $body.html('');
 
   var index = streams.home.length - 1;
   while(index >= 0){
@@ -8,7 +7,7 @@ $(document).ready(function(){
     var $name = $('<div class="name"></div>');
     var $message = $('<div class="message"></div>');
     //show username and date
-    $name.text('@' + tweet.user + ' created at ' + tweet.created_at);
+    $name.text('@' + tweet.user + ' created at ' + tweet.created_at.getHours() + ':' + tweet.created_at.getMinutes());
     $name.appendTo($body);
     //show message
     $message.text(tweet.message);
@@ -16,5 +15,4 @@ $(document).ready(function(){
     index -= 1;
   }
   
-
 });
