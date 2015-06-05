312c43c61f8eb997920f05b5c4fb18159eb2392c
Charles Crame <ccrame@gmail.com>
2015-06-04 23:02:07 -0700

    more styling, separate lines for name and message

--- twittler.js
+++ twittler.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
-  var $body = $('.tweets');
+  var $body = $('.tweet-board');
   $body.html('');
 
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
+    var $name = $('<div class="name"></div>');
+    var $message = $('<div class="message"></div>');
+    //show username and date
+    $name.text('@' + tweet.user + ' created at ' + tweet.created_at);
+    $name.appendTo($body);
+    //show message
+    $message.text(tweet.message);
+    $message.appendTo($body);
     index -= 1;
   }
   
