8385d2c5c24008ee3b962ad9d526adad533ca2c4
Charles Crame <ccrame@gmail.com>
2015-06-06 13:54:19 -0700

    Added profile section in index.html, profile styling in main.css, elapsed time function for posts in twittler.js

--- twittler.js
+++ twittler.js
@@ -5,9 +5,31 @@ $(document).ready(function(){
      $('.tweet').html('');
   };
 
-  var getTime = function(){
+  var postTimeElapsed = function(timeInput){
+    var seconds = Math.floor(((new Date).getTime() - timeInput.getTime()) / 1000);
+    var minutes = Math.floor(seconds/60);
+    var hours = Math.floor(minutes/ 60);
+    var days = Math.floor(hours/24);
 
-  }//end of getTime function
+    if(days > 1){
+      return 'posted ' + days + 'days ago';
+    } else if (days === 1) {
+      return 'posted 1 day ago';
+    } else if (hours > 1) {
+      return 'posted ' + hours + ' hours ago';
+    } else if ( hours === 1) {
+      return 'posted 1 hour ago';
+    } else if ( minutes > 1) {
+      return 'posted ' + minutes + ' minutes ago';
+    } else if (minutes === 1) {
+      return 'posted 1 minute ago';
+    } else if (seconds > 1) {
+      return 'posted ' + seconds + ' seconds ago';
+    } else {
+      return 'posted 1 second ago';
+    }
+
+  };//end of getTime function
 
   var loadScreen = function(){
     index = streams.home.length - 1;
@@ -15,9 +37,11 @@ $(document).ready(function(){
     $('.tweet-board').append($('<div class="messages"></div>'));
     while(index >= 0){
       var tweet = streams.home[index];
+      var time = postTimeElapsed(tweet.created_at);
       var block= $('<div class="tweet"></div>')
       var pic = $('<img class="profile picture" src="user.png">');
-      var header = $('<p class="message-header">@' + tweet.user + '  created at ' + tweet.created_at.getHours() + ':' + tweet.created_at.getMinutes() + '</p>');
+      var header = $('<p class="message-header">@' + tweet.user + ' ' + '</p>');
+      header.append(time);
       var message = $('<p>' + tweet.message + '</p>');
       //show username and date
       block.append(pic);
